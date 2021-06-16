1aa749acb24371db990abbc75cd6cd9bcb31161c
1smat <reven13022002@gmail.com>
2021-06-16 13:01:41 +0500

    done

---
+++ src/data/users.js
@@ -0,0 +1,104 @@
+const users = [
+  {
+    id: 1,
+    name: "Ismatullo",
+    username: "ism4tullo",
+  },
+  {
+    id: 2,
+    name: "Shaha Dolimov",
+    username: "shahadolimov",
+  },
+  {
+    id: 3,
+    name: "Apple",
+    username: "appple",
+  },
+  {
+    id: 4,
+    name: "JavaScript",
+    username: "javascript",
+  },
+  {
+    id: 5,
+    name: "Madrimov Doston",
+    username: "madrimov_doston",
+  },
+  {
+    id: 6,
+    name: "Abdulfayz Turdiyev",
+    username: "_fayz9703",
+  },
+  {
+    id: 7,
+    name: "Diyorbek Tuynazarov",
+    username: "diyorbek_18_02",
+  },
+  {
+    id: 8,
+    name: "Tesla",
+    username: "tesla",
+  },
+  {
+    id: 9,
+    name: "TechBrain Academy",
+    username: "techbrain.academy",
+  },
+  {
+    id: 10,
+    name: "Akmal Kadirov",
+    username: "kadirovdev",
+  },
+  {
+    id: 11,
+    name: "Pepsi Cola",
+    username: "pepsi",
+  },
+  {
+    id: 12,
+    name: "Eminem",
+    username: "eminem",
+  },
+  {
+    id: 13,
+    name: "Muhammadali Eshonqulov",
+    username: "muhammadali_eshonqulov",
+  },
+  {
+    id: 14,
+    name: "gdg Tashkent",
+    username: "gdg_tashkent",
+  },
+  {
+    id: 15,
+    name: "React JS",
+    username: "react.official",
+  },
+  {
+    id: 16,
+    name: "Linux tech tips",
+    username: "Linux_tech",
+  },
+  {
+    id: 17,
+    name: "Miyagi",
+    username: "miyagi",
+  },
+  {
+    id: 18,
+    name: "Pudge Butcher",
+    username: "pudge",
+  },
+  {
+    id: 19,
+    name: "Rowan Atkinson",
+    username: "Mr Bean",
+  },
+  {
+    id: 20,
+    name: "Vladilen Minin",
+    username: "vladilen.minin",
+  },
+];
+
+export default users;
